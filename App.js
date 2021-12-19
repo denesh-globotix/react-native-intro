da55dba4c64b6b6404939fb6c6203882b48eab18
globotix <denesh.siva@globotix.sg>
2021-12-19 11:35:27 +0800

    Uploading all files

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Button
+        title="Learn More"
+        accessibilityLabel="Learn more about this purple button"
+        onPress={() => {
+          Alert.alert("You clicked me!")
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
